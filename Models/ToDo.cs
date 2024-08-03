fe021924d1541a87f035ef3588deb716a39db330
Mohamed Zakaria <132154144+moziko00@users.noreply.github.com>
2024-08-03 13:38:09 +0300

    Add project files.

---
+++ Models/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+
+    }
+}
