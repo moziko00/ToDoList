732399951975d09eadd2d10c97fc158860cb6a01
Mohamed Zakaria <132154144+moziko00@users.noreply.github.com>
2024-08-03 19:05:02 +0300

    To Do Project

---
+++ Controllers/ToDoItemsController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Data;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoItemsController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public IActionResult Items()
+        {
+            var name = Request.Cookies["ToDoItemName"];
+            ViewBag.Cookie = name;
+            var result = context.toDos.ToList();
+            return View(result);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string Name)
+        {
+
+            var cookieOptions = new CookieOptions
+            {
+                Expires = DateTime.UtcNow.AddDays(1)
+            };
+            Response.Cookies.Append("ToDoItemName", Name, cookieOptions);
+
+            return RedirectToAction("Items");
+        }
+
+
+        public IActionResult CreateNew()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateNew(ToDo toDo)
+        {
+            TempData["SuccessMessage"] = $"Created {toDo.Title} successfully";
+            context.toDos.Add(toDo);
+            context.SaveChanges();
+
+
+            return RedirectToAction("Items");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var result = context.toDos.Find(id);
+            if (result == null)
+            {
+                return RedirectToAction("NotFound");
+            }
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult Edit(ToDo toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingToDo = context.toDos.Find(toDo.Id);
+                if (existingToDo == null)
+                {
+                    return RedirectToAction("NotFound");
+                }
+
+                // Update fields
+                existingToDo.Title = toDo.Title;
+                existingToDo.Description = toDo.Description;
+                existingToDo.Deadline = toDo.Deadline;
+
+                context.toDos.Update(existingToDo);
+                context.SaveChanges();
+
+                return RedirectToAction("Items");
+            }
+
+            // If the model state is invalid, return the view with the current model.
+            return View(toDo);
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var result = context.toDos.Find(id);
+
+            if (result != null)
+            {
+                context.toDos.Remove(result);
+                context.SaveChanges();
+                return RedirectToAction("Items");
+            }
+            else
+                return RedirectToAction("NotFound");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet <ToDo> toDos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetConnectionString("DefaultConnetion");
+
+
+            optionsBuilder.UseSqlServer(builder);
+
+
+        }
+    }
+}
